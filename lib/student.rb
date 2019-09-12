e70c67c04a9b488a87bf4870f66e6da3cf894fb8
Steven Hunal <stevehunal@gmail.com>
2019-09-12 01:23:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,12 +8,10 @@ class Student
   attr_accessor :name, :grade
   
   attr_reader :id
-  @@all = []
   def initialize(name, grade, id=nil)
     @id = id
     @name = name
     @grade = grade
-    @@all << self
   end
   
   def self.create_table
